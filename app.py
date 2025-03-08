59465165832eb6cd796a56570cc138afc91e79fd
Mohmmad hissyn Aon (ENDUP) <hhfw809o@gmail.com>
2025-03-08 18:42:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -59,8 +59,5 @@ def login():
 if __name__ == '__main__':
     init_db()
     
-    # Use environment variables for deployment configurations
-    host = os.getenv('FLASK_HOST', '0.0.0.0')
-    port = int(os.getenv('FLASK_PORT', 5000))
-    
-    app.run(host=host, port=port)
+  
+    app.run()
